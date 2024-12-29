86d1c65e1650692cf7cfc07bbc771acc552f3b20
KidaiRinboku <kidairinboku@gmail.com>
2024-12-29 23:15:35 +0900

    Add files via upload

---
+++ DatabaseManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using SQLite;
+using System.IO;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private SQLiteConnection db;
+
+    void Start()
+    {
+        // データベースファイルへのパス
+        var dbPath = Path.Combine(Application.persistentDataPath, "game.db");
+        var db = new SQLiteConnection(dbPath);
+        db.Execute("PRAGMA foreign_keys = ON");
+
+        // テーブルの作成
+        db.CreateTable<Item>();
+
+        // データの挿入
+        db.Insert(new Item {Name = "Dummy Item1" });
+        db.Insert(new Item {Name = "Dummy Item2" });
+        db.Insert(new Item {Name = "Dummy Item111" });
+        // データの読み取りと表示
+        var items = db.Table<Item>().ToList();
+        items.ForEach(item => Debug.Log($"Item: {item.Id}, {item.Name}"));
+        
+        // テーブル削除
+       // db.DropTable<Item>();
+
+        db.Close();
+
+    }
+
+    // void OnDestroy()
+    // {
+
+    //     if (db != null)
+    //     {
+            
+    //     }
+    // }
+}
+
+public class Item
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
